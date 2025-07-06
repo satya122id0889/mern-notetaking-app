f3dbdf585ec6ae50d652756ada45f949c9a2eb6f
Satyabrata Satapathy <satya78550@gmail.com>
2025-07-06 22:29:53 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import notesRoutes from '../src/routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
@@ -13,13 +14,16 @@ console.log(process.env.MONGO_URI);
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 
 
 // Middleware to parse JSON requests
-app.use(cors(
-    {origin: "http://localhost:5173",}
-));
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors(
+        {origin: "http://localhost:5173",}
+    ));
+}
 app.use(express.json());
 app.use(ratelimiter)
 
@@ -29,6 +33,16 @@ app.use(ratelimiter)
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend" , 'dist' , 'index.html'));
+    })
+}
+
+
 
 connectDB().then(() => {
     app.listen(
